5efcf682cb06a15274fa3df50915758481ee4e83
cuebin <beknosee@outlook.com>
2023-05-11 20:56:29 +0200

    .

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 int main(int argc, char **argv) {
 
-    if(argc > 3 || argc < 2) {
+    if(argc != 3) {
         printf("Usage: <len> <u><l><s><n>\n");
         return 1;
     }
@@ -17,20 +17,24 @@ int main(int argc, char **argv) {
     char lower[] = "abcdefghijklmnopqrstuvwxyz";
     char symbols[] = "~`!@#$%^&*()_-+={[}]|\\:;'<,>.?/";
     char numbers[] = "0123456789";
-    char alphabet[256];
+    char alphabet[96];
 
     for(int i = 0; i < strlen(argv[2]); i++) {
         if(argv[2][i] == 'u') {
             strcat(alphabet, upper);
+            upper[0] = '\0';
         }
         else if(argv[2][i] == 'l') {
             strcat(alphabet, lower);
+            lower[0] = '\0';
         }
         else if(argv[2][i] == 's') {
             strcat(alphabet, symbols); 
+            symbols[0] = '\0';
         }
         else if(argv[2][i] == 'n') {
             strcat(alphabet, numbers);
+            numbers[0] = '\0';
         }
     }
 
